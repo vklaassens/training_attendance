25ada67d071db374642d08e25d58ba0fd999ebae
Vivienne <vklaassens@gmail.com>
2017-06-24 13:31:50 +0200

    updated with update and destroy functionality

--- training_attendance/app/controllers/trainings_controller.rb
+++ training_attendance/app/controllers/trainings_controller.rb
@@ -12,6 +12,10 @@ def new
   @training = Training.new
 end
 
+def edit
+  @training = Training.find(params[:id])
+end
+
 def create
 
     @training = Training.new(params.require(:training).permit(:date, :location))
@@ -20,4 +24,26 @@ def create
     else render 'new'
     end
 end
+
+def update
+  @training = Training.find(params[:id])
+
+  if @training.update(training_params)
+    redirect_to @training
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @training = Training.find(params[:id])
+  @training.destroy
+
+  redirect_to trainings_path
+end
+
+private
+  def training_params
+    params.require(:training).permit(:date, :location)
+  end
 end
